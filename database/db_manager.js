44a5084afba32ba20eb079c80b6416c75473809e
Zach Gillis <zgillis11@gmail.com>
2020-06-21 20:55:48 -0500

    Add 5-minute Portfolio DB Insert

--- database/db_manager.js
+++ database/db_manager.js
@@ -1,6 +1,6 @@
 const {Sequelize} = require('sequelize')
 const PortfolioHistoryModel = require('./portfolio_history')
-
+const btc_client = require('../btc_client')
 const sequelize = new Sequelize("mysql://zgillis:laptop10@localhost:4406/zbinancebot");
 
 sequelize.authenticate().then(() => {
@@ -12,11 +12,22 @@ sequelize.authenticate().then(() => {
 
 const PortfolioHistory = PortfolioHistoryModel(sequelize, Sequelize)
 
-sequelize.sync({force: true}).then(() => {
+sequelize.sync({force: false}).then(() => {
     console.log("Database and tables created successfully.")
+    setInterval(() => {
+        btc_client.get_portfolio().then((balance_info) => {
+            PortfolioHistory.create({
+                date: Date.now(),
+                balances: balance_info.balances,
+                totalValue: balance_info.total_value
+            })
+            
+        })
+    }, 300000)
 })
 
 
 module.exports = {
-    sequelize
+    sequelize,
+    PortfolioHistory
 }
